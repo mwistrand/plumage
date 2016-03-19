8436d4ab81766211931aef0f01ebfea1d54996b7
Matt Wistrand <matt@troglodytidae.com>
2016-03-19 14:29:53 -0500

    Implement node tree parsing.
    Add a template tag function that accepts an ES2015 template and returns a tree hierarchy of node definitions.

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,81 @@
-const BLANK_PATTERN = /^\s+$/m;
+const BLANK_PATTERN = /^\s+$/;
+const TAG_NAME_PATTERN = /^<\/?([^\s\/>]+).*/;
+
+function convertTemplateToArray(literals: string[], values: any[]): any[] {
+	const tags: any[] = [];
+	let inAttributeValue = false;
+	let inComment = false;
+	let inTag = false;
+	let result = '';
+
+	let char: string;
+	let previous: string;
+
+	literals.forEach((literal) => {
+		let index = 0;
+
+		while (index < literal.length) {
+			char = literal.charAt(index);
+
+			if (char === '<' && !inAttributeValue) {
+				if (result) {
+					if (!isWhitespace(result)) {
+						if (typeof result === 'string') {
+							result = result.trim();
+						}
+						tags.push(result);
+					}
+					result = '';
+				}
+				inTag = true;
+				result += char;
+			}
+			else if (inTag && char === '>' && !inAttributeValue) {
+				inTag = false;
+				result += char;
+				tags.push(result);
+				result = '';
+			}
+			else if (char === '/' && previous === '/') {
+				inComment = true;
+			}
+			else if (inComment && char === '\n') {
+				inComment = false;
+				result = '';
+			}
+			else if (!inComment) {
+				if (inTag) {
+					if (char === '"' && previous === '=') {
+						inAttributeValue = true;
+					}
+					else if (char === '"' && inAttributeValue && previous !== '\\') {
+						inAttributeValue = false;
+					}
+					if (previous === '<' && !inAttributeValue && isWhitespace(char)) {
+						inTag = false;
+						result += char;
+					}
+					else {
+						result += char;
+					}
+				}
+				else {
+					result += char;
+				}
+			}
+
+			previous = char;
+			index += 1;
+		}
+
+		if (values.length && !inComment) {
+			const nextValue: any = values.shift();
+			tags.push(mapTaggedValue(nextValue));
+		}
+	});
+
+	return tags;
+}
 
 function extractAttributeValue(parts: string[]): string {
 	if (!parts || !parts.length) {
@@ -18,9 +95,153 @@ function extractAttributeValue(parts: string[]): string {
 	return value;
 }
 
+// TODO: This functionality could likely be handled during the `nestNodeArray`
+// operations, which would remove the additional traverse. `nestNodeArray` could be
+// updated to accept an additional function that converts values in-place to their
+// final format.
+function mapArrayToNode(parts: any[]): HtmlMap {
+	const first: any = parts[0];
+	if (!isHtmlTag(first)) {
+		throw new SyntaxError('The root value must be an HTML element.');
+	}
+
+	const root = mapHtmlTagToArray(first);
+
+	if (parts.length === 1) {
+		return root;
+	}
+
+	root.children = [];
+	for (let i = 1; i < parts.length; i += 1) {
+		const item: any = parts[i];
+		let result: any;
+
+		if (Array.isArray(item)) {
+			result = mapArrayToNode(item);
+			root.children.push(result);
+		}
+		else if (typeof item !== 'string' || isWhitespace(item)) {
+			root.children.push(item);
+		}
+		else if (isHtmlTag(item)) {
+			result = mapHtmlTagToArray(item);
+			root.children.push(result);
+		}
+		else {
+			root.children.push(item);
+		}
+	}
+
+	return root;
+}
+
+function mapTaggedValue(value: any): string | TypeMap {
+	if (typeof value === 'string') {
+		return value;
+	}
+	else if (typeof value === 'number') {
+		return String(value);
+	}
+	else if (Array.isArray(value)) {
+		return {
+			type: ValueTypes.Array,
+			value: value
+		};
+	}
+	else if (typeof value === 'symbol') {
+		return value.toString();
+	}
+	else if (typeof value === 'function') {
+		return {
+			type: ValueTypes.Function,
+			value: value
+		};
+	}
+	else if (typeof value === 'undefined') {
+		return {
+			type: ValueTypes.Undefined,
+			value: value
+		};
+	}
+	else {
+		return {
+			type: ValueTypes.Object,
+			value: value
+		};
+	}
+}
+
+function nestNodeArray(parts: any[]): any[] {
+	const closingTags: string[] = [];
+	const nested: { [key: string]: any[] } = {
+		0: []
+	};
+	let current: any = nested[0];
+	let i = parts.length - 1;
+	let nestLevel = 0;
+
+	let currentClosingTag: string;
+	let part: any;
+
+	if (parts.length > 1) {
+		part = parts[i];
+
+		if (typeof part !== 'string' || part.indexOf('</') !== 0) {
+			throw new SyntaxError('The template must end with a closing tag, ' +
+				'or must contain only a single tag or object.');
+		}
+
+		currentClosingTag = parseTagName(part);
+		i -= 1;
+	}
+
+	while (i >= 0) {
+		part = parts[i];
+
+		if (typeof part !== 'string') {
+			current.unshift(part);
+		}
+		else if (part.indexOf('</') === 0) {
+			closingTags.push(currentClosingTag);
+			nestLevel += 1;
+			current = nested[nestLevel] = [];
+			currentClosingTag = parseTagName(part);
+		}
+		else if (isHtmlTag(part)) {
+			if (parseTagName(part) === currentClosingTag) {
+				current.unshift(part);
+
+				if (nestLevel > 0) {
+					delete nested[nestLevel];
+					nestLevel -= 1;
+					nested[nestLevel].unshift(current);
+					current = nested[nestLevel];
+					currentClosingTag = closingTags.pop();
+				}
+			}
+			else {
+				// Assume that the tag is never closed.
+				current.unshift(part);
+			}
+		}
+		else {
+			current.unshift(part);
+		}
+
+		i -= 1;
+	}
+
+	return nested[0];
+}
+
+function parseTagName(tag: string): string {
+	return tag.replace(TAG_NAME_PATTERN, '$1');
+}
+
 export interface HtmlMap {
 	name: string;
-	attributes: { [name: string]: string };
+	attributes?: { [name: string]: string };
+	children?: any[];
 }
 
 /**
@@ -47,8 +268,13 @@ export const escapeHtml = (function () {
 	};
 })();
 
+export function isHtmlComment(value: string): boolean {
+	return value.indexOf('<!--') === 0 && value.lastIndexOf('-->') === value.length - 3;
+}
+
 /**
- * Determines whether the specified string represents an HTML tag.
+ * Determines whether the specified string represents an HTML tag; HTML comments
+ * are considered to not be valid HTML tags.
  *
  * @param value
  * The candidate HTML tag.
@@ -57,7 +283,7 @@ export const escapeHtml = (function () {
  * `true` if the value is an HTML tag; `false` otherwise.
  */
 export function isHtmlTag(value: string): boolean {
-	return value.length >= 3 && value.charAt(0) === '<' &&
+	return !isHtmlComment(value) && value.length >= 3 && value.charAt(0) === '<' &&
 		value.charAt(value.length - 1) === '>' && !isWhitespace(value.charAt(1));
 }
 
@@ -138,10 +364,11 @@ export function mapHtmlTagToArray(html: string): HtmlMap {
 	previous && fragments.push(previous);
 	current && fragments.push(current);
 
-	const map: HtmlMap = {
-		name: fragments[0],
-		attributes: {},
-	};
+	const map: HtmlMap = { name: fragments[0] };
+
+	if (fragments.length > 1) {
+		map.attributes = {};
+	}
 
 	for (let j = fragments.length - 1; j >= 1; --j) {
 		let attrParts: string[] = fragments[j].split('=');
@@ -157,3 +384,119 @@ export function mapHtmlTagToArray(html: string): HtmlMap {
 
 	return map;
 }
+
+// {
+// 	name: 'div',
+// 	attributes: {},
+// 	type: 'node',
+
+// 	children: [
+// 		{
+// 			type: 'array',
+// 			value: []
+// 		},
+
+// 		{
+// 			type: 'node',
+// 			name: 'span',
+// 			children: [
+// 				'lorem ipsum',
+// 				'<!-- comment -->',
+// 				'text with an <!-- inline --> comment'
+// 			]
+// 		},
+// 		{
+//			type: 'object',
+//			value: {}
+//		}
+// 	]
+// }
+
+/**
+ * A tag function that converts an ES2015 template into a node tree.
+ *
+ * Returns an map representing a hierarchy of HTML node definitions. The values
+ * contained within the map are not converted to DOM nodes. Instead, it is up to
+ * the consuming API to correctly render the values. Each HTML tag is converted to
+ * an object with a `name` property and an optional `attributes` object and `children`
+ * array. Node attribute names must be unique; duplicate attribute names will cause
+ * a `SyntaxError` to be thrown.
+ *
+ * HTML tags need not be closed; but any unclosed tag is assumed to not have any
+ * children. HTML comments (`<!-- ... -->`) are preserved in the output, while line
+ * comments (`// ...`) are stripped out.
+ *
+ * Arbitrary values included in the interpolation expressions are mapped accordingly:
+ *
+ * - Strings, numbers, and symbols are converted to strings.
+ * - All other values are converted to an `TypeMap` object.
+ *
+ * Use: parseNodeTree`<div>...</div>`.
+ * Example:
+ *
+ * const arbitraryObject = { prop: 'value' };
+ * const record = {
+ *	name: 'The Complete Village Vanguard Recordings',
+ *	artist: 'Bill Evans'
+ * };
+ * parseNodeTree`<form class="class1 class2" onsubmit="methodName">
+ *	${arbitraryObject}
+ *	<fieldset>
+ *		<input name="artist" value="${record.artist}">
+ *		<input type="submit" disabled>
+ *	</fieldset>
+ * </form>`
+ *
+ * Converts to:
+ *
+ * {
+ *	name: 'form',
+ *	attributes: {
+ *		'class': 'class1 class2'
+ *		onsubmit: 'methodName'
+ *	},
+ *	children: [
+ *		{
+ *			type: 0, // ValueTypes.Object
+ *			value: { prop: 'value' }
+ *		},
+ *		{
+ *			name: 'fieldset',
+ *			children: [
+ *				{
+ *					name: 'input',
+ *					attributes: { name: 'artist', value: 'Bill Evans' }
+ *				},
+ *				{
+ *					name: 'input',
+ *					attributes: { type: 'submit', disabled: null }
+ *				}
+ *			]
+ *		}
+ *	]
+ * }
+ */
+export function parseNodeTree(parts: string[], ...values: any[]): HtmlMap {
+	return mapArrayToNode(nestNodeArray(convertTemplateToArray(parts, values)));
+}
+
+/**
+ * An object map representing objects, arrays, functions, and undefined values
+ * included in the template via interpolation expressions.
+ */
+export interface TypeMap {
+	type: ValueTypes,
+	value: any
+}
+
+/**
+ *
+ * TODO: On the one hand, an enum seems appropriate as the underlying values never
+ * change. On the other hand, using string representations seems much simpler.
+ */
+export const enum ValueTypes {
+	Object,
+	Array,
+	Function,
+	Undefined
+};
--- tests/unit/parser.ts
+++ tests/unit/parser.ts
@@ -47,23 +47,19 @@ registerSuite({
 	mapHtmlTagToArray: {
 		'assert tag with no attributes'() {
 			assert.deepEqual(parser.mapHtmlTagToArray('<input />'), {
-				name: 'input',
-				attributes: {}
+				name: 'input'
 			});
 
 			assert.deepEqual(parser.mapHtmlTagToArray('<div>'), {
-				name: 'div',
-				attributes: {}
+				name: 'div'
 			});
 
 			assert.deepEqual(parser.mapHtmlTagToArray('<random:value>'), {
-				name: 'random:value',
-				attributes: {}
+				name: 'random:value'
 			});
 
 			assert.deepEqual(parser.mapHtmlTagToArray('<random-value>'), {
-				name: 'random-value',
-				attributes: {}
+				name: 'random-value'
 			});
 		},
 
@@ -118,6 +114,193 @@ registerSuite({
 					disabled: null
 				}
 			}, 'Multiline support.');
+		}
+	},
+
+	parseNodeTree: {
+		errors() {
+			assert['throws'](function () {
+				const name = 'Bill Evans';
+				parser.parseNodeTree`${name}`;
+			});
+			assert['throws'](function () {
+				parser.parseNodeTree`<!-- comment -->`;
+			});
+		},
+
+		'assert single, unclosed node'() {
+			const result = parser.parseNodeTree`<input name="name" disabled>`;
+			assert.deepEqual(result, {
+				name: 'input',
+				attributes: {
+					name: 'name',
+					disabled: null
+				}
+			});
+		},
+
+		'assert single, closed node'() {
+			const result = parser.parseNodeTree`<div class="Component-element"
+				data-text="The quick brown \"fox\" jumped over the lazy dog."
+				data-other-text="2 + 2 = 4"
+				data-plumage-id="12345" id="12345" aria-disabled="disabled" disabled></div>`;
+
+			assert.deepEqual(result, {
+				name: 'div',
+				attributes: {
+					'class': 'Component-element',
+					'data-text': 'The quick brown "fox" jumped over the lazy dog.',
+					'data-other-text': '2 + 2 = 4',
+					'data-plumage-id': '12345',
+					id: '12345',
+					'aria-disabled': 'disabled',
+					disabled: null
+				}
+			});
+		},
+
+		'assert text nodes'() {
+			const result = parser.parseNodeTree`<div>Lorem ipsum dolor sit amet</div>`;
+
+			assert.deepEqual(result, {
+				name: 'div',
+				children: [ 'Lorem ipsum dolor sit amet' ]
+			});
+		},
+
+		'assert comments'() {
+			let result = parser.parseNodeTree`<div><!-- html comment --></div>`;
+			assert.deepEqual(result, {
+				name: 'div',
+				children: [ '<!-- html comment -->' ]
+			}, 'HTML comments are preserved.');
+
+			result = parser.parseNodeTree`<div>
+				// line comment
+			</div>`;
+			assert.deepEqual(result, {
+				name: 'div'
+			}, 'Line comments are removed.');
+		},
+
+		'assert multiple tags'() {
+			const result = parser.parseNodeTree`<div onclick="methodName">
+				// HTML comment
+				<span>$![someProperty]</span>
+				<input name="happy">
+				Some text
+			</div>`;
+
+			assert.deepEqual(result, {
+				name: 'div',
+				attributes: { onclick: 'methodName' },
+				children: [
+					{
+						name: 'span',
+						children: [ '$![someProperty]' ]
+					},
+					{
+						name: 'input',
+						attributes: { name: 'happy' }
+					},
+
+					'Some text'
+				]
+			});
+		},
+
+		'replacement values': {
+			'string'() {
+				const name = 'Bill Evans';
+				const result = parser.parseNodeTree`<div>
+					${name}
+				</div>`;
+
+				assert.deepEqual(result, {
+					name: 'div',
+					children: [ 'Bill Evans' ]
+				});
+			},
+
+			'number'() {
+				const count = 42;
+				const result = parser.parseNodeTree`<div>
+					${count}
+				</div>`;
+
+				assert.deepEqual(result, {
+					name: 'div',
+					children: [ '42' ]
+				});
+			},
+
+			'symbol'() {
+				const sym = Symbol();
+				const result = parser.parseNodeTree`<div>
+					${sym}
+				</div>`;
+
+				assert.deepEqual(result, {
+					name: 'div',
+					children: [ sym.toString() ]
+				});
+			},
+
+			'array'() {
+				const array = [ 0, 1, 2 ];
+				const result = parser.parseNodeTree`<div>
+					${array}
+				</div>`;
+
+				assert.deepEqual(result, {
+					name: 'div',
+					children: [
+						{ type: parser.ValueTypes.Array, value: array }
+					]
+				});
+			},
+
+			'object'() {
+				const object = { name: 'Bill Evans', album: 'New Jazz Conceptions' };
+				const result = parser.parseNodeTree`<div>
+					${object}
+				</div>`;
+
+				assert.deepEqual(result, {
+					name: 'div',
+					children: [
+						{ type: parser.ValueTypes.Object, value: object }
+					]
+				});
+			},
+
+			'function'() {
+				const callback = function () {};
+				const result = parser.parseNodeTree`<div>
+					${callback}
+				</div>`;
+
+				assert.deepEqual(result, {
+					name: 'div',
+					children: [
+						{ type: parser.ValueTypes.Function, value: callback }
+					]
+				});
+			},
+
+			'undefined'() {
+				let value: any;
+				const result = parser.parseNodeTree`<div>
+					${value}
+				</div>`;
+
+				assert.deepEqual(result, {
+					name: 'div',
+					children: [
+						{ type: parser.ValueTypes.Undefined, value: undefined }
+					]
+				});
+			}
 		}
 	}
 });
